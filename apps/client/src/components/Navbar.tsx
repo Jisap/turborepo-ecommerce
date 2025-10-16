33fee974e9ad0cac20f01a3d38c0e826a1098b1d
Jorge Matos <jisapjisap@gmail.com>
2025-10-16 12:15:11 +0200

    feat(client): add OrdersPage component and ProfileButton for user order navigation in Navbar

--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import SearchBar from "./SearchBar";
 import { Bell, Home, ShoppingCart } from "lucide-react";
 import ShoppingCartIcon from "./ShoppingCartIcon";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import ProfileButton from "./ProfileButton";
 
 const Navbar = () => {
   return (
@@ -38,7 +39,7 @@ const Navbar = () => {
         </SignedOut>
 
         <SignedIn>
-          <UserButton />
+          <ProfileButton />
         </SignedIn>
       </div>
     </nav>
