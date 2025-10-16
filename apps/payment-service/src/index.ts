197af8053ebf622759ca734f431d8d27a2ace4d1
Jorge Matos <jisapjisap@gmail.com>
2025-10-16 16:38:17 +0200

    feat(services): implement user authentication middleware for test endpoint in payment service

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -4,6 +4,7 @@ import { Hono } from 'hono'
 import { uptime } from 'process'
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { cors } from 'hono/cors';
+import { shouldBeUser } from './middleware/authMiddleware.js';
 
 const app = new Hono();
 
@@ -20,17 +21,10 @@ app.get('/health', (c) => {
 })
 
 
-app.get('/test', (c) => {
-  const auth = getAuth(c);
-  if(!auth?.userId) {
-    return c.json({
-      message: "You are not logged in!"
-    })
-  }
-
+app.get('/test', shouldBeUser, (c) => {
   return c.json({
     message: "Payment service is authenticated!",
-    userId: auth.userId
+    userId: c.get("userId")
   })
 
 })
