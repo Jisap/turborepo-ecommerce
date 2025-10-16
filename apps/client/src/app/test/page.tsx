9988c69a8ee0517db00a422939aa754caac26ecd
Jorge Matos <jisapjisap@gmail.com>
2025-10-16 16:07:39 +0200

    feat(services): add @clerk/fastify dependency and update test page to fetch product and order data

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -5,16 +5,27 @@ const TestPage = async() => {
 
   const { getToken } = await auth();
   const token = await getToken();
-  console.log(token);
 
-  const res = await fetch("http://localhost:8000/test", {
+  const resProduct = await fetch("http://localhost:8000/test", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  
+  const dataProduct = await resProduct.json() ;
+  console.log("data-Product",dataProduct);
+  
+  const resOrder = await fetch("http://localhost:8001/test", {
     headers: {
       Authorization: `Bearer ${token}`
     }
   });
  
-  const data = await res.json() ;
-  console.log("data",data);
+  const dataOrder = await resOrder.json() ;
+  console.log("data-Order",dataOrder);
+
+
+
 
   return (
     <div>test</div>
--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors({
   origin: ["http://localhost:3002", "http://localhost:3003"],
   credentials: true,
+  allowedHeaders: ["Authorization", "Content-Type"]
 }));
 
 app.use(express.json());
