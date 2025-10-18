e7af0196511eb19192882cef8935113dfc2e74cd
Jorge Matos <jisapjisap@gmail.com>
2025-10-18 16:18:04 +0200

    fix(product-service): enhance error handling and restore validation in product creation; remove admin check for category creation

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -6,22 +6,22 @@ import { prisma, Prisma } from "@repo/product-db";
 export const createProduct = async (req: Request, res: Response) => {
   const data: Prisma.ProductCreateInput = req.body;
 
-  // const { colors, images } = data;
-  // if (!colors || !Array.isArray(colors) || colors.length === 0) {
-  //   return res.status(400).json({ message: "Colors array is required!" });
-  // }
+  const { colors, images } = data;
+  if (!colors || !Array.isArray(colors) || colors.length === 0) {
+    return res.status(400).json({ message: "Colors array is required!" });
+  }
 
-  // if (!images || typeof images !== "object") {
-  //   return res.status(400).json({ message: "Images object is required!" });
-  // }
+  if (!images || typeof images !== "object") {
+    return res.status(400).json({ message: "Images object is required!" });
+  }
 
-  // const missingColors = colors.filter((color) => !(color in images));
+  const missingColors = colors.filter((color) => !(color in images));
 
-  // if (missingColors.length > 0) {
-  //   return res
-  //     .status(400)
-  //     .json({ message: "Missing images for colors!", missingColors });
-  // }
+  if (missingColors.length > 0) {
+    return res
+      .status(400)
+      .json({ message: "Missing images for colors!", missingColors });
+  }
 
   const product = await prisma.product.create({ data });
 
--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { clerkMiddleware, getAuth } from '@clerk/express';
 import { shouldBeUser } from './middleware/authMiddleware';
@@ -31,8 +31,15 @@ app.get("/test", shouldBeUser, (req, res) => {
   res.json({ message: "Product service authenticated", userId: req.userId });
 });
 
-app.use("/products", productRouter)
-app.use("/categories", categoryRouter)
+app.use("/products", productRouter);
+app.use("/categories", categoryRouter);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  return res
+    .status(err.status || 500)
+    .json({ message: err.message || "Inter Server Error!" });
+});
 
 app.listen(8000, () => {
   console.log("Product service is running on port 8000");
