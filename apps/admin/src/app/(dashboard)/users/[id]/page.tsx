c9788a11b03c11f96923c4185132cdb285e13898
Jorge Matos <jisapjisap@gmail.com>
2025-10-31 11:26:58 +0100

    feat(admin): implement user detail page with dynamic data fetching from Clerk, enhance user display with real-time information, and improve layout for better user experience

--- apps/admin/src/app/(dashboard)/users/[id]/page.tsx
+++ apps/admin/src/app/(dashboard)/users/[id]/page.tsx
@@ -20,10 +20,48 @@ import { Button } from "@/components/ui/button";
 import EditUser from "@/components/EditUser";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import AppLineChart from "@/components/AppLineChart";
+import { auth, User } from "@clerk/nextjs/server";
 
-const SingleUserPage = () => {
+
+const getData = async (id: string): Promise<User | null> => {
+  const { getToken } = await auth();
+  const token = await getToken();
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/users/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+    return data;
+
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+const SingleUserPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+
+  const { id } = await params;
+  const data = await getData(id);
+
+  if (!data) {
+    return <div className="">User not found!</div>;
+  }
+
   return (
     <div className="">
+      {/* Breadcrumbs */}
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
@@ -35,15 +73,18 @@ const SingleUserPage = () => {
           </BreadcrumbItem>
           <BreadcrumbSeparator />
           <BreadcrumbItem>
-            <BreadcrumbPage>John Doe</BreadcrumbPage>
+            <BreadcrumbPage>
+              {data?.firstName + " " + data?.lastName || data?.username || "-"}
+            </BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      {/* CONTAINER */}
+
+      {/* Container principal */}
       <div className="mt-4 flex flex-col xl:flex-row gap-8">
-        {/* LEFT */}
+        {/* LEFT COLUMN*/}
         <div className="w-full xl:w-1/3 space-y-6">
-          {/* USER BADGES CONTAINER */}
+          {/* User badges */}
           <div className="bg-primary-foreground p-4 rounded-lg">
             <h1 className="text-xl font-semibold">User Badges</h1>
             <div className="flex gap-4 mt-4">
@@ -106,14 +147,23 @@ const SingleUserPage = () => {
               </HoverCard>
             </div>
           </div>
-          {/* USER CARD CONTAINER */}
+
+          {/* User Avatar and description  */}
           <div className="bg-primary-foreground p-4 rounded-lg space-y-2">
             <div className="flex items-center gap-2">
               <Avatar className="size-12">
-                <AvatarImage src="https://avatars.githubusercontent.com/u/1486366" />
-                <AvatarFallback>JD</AvatarFallback>
+                <AvatarImage src={data.imageUrl} />
+                <AvatarFallback>
+                  {data?.firstName?.charAt(0) ||
+                    data?.username?.charAt(0) ||
+                    "-"}
+                </AvatarFallback>
               </Avatar>
-              <h1 className="text-xl font-semibold">John Doe</h1>
+              <h1 className="text-xl font-semibold">
+                {data?.firstName + " " + data?.lastName ||
+                  data?.username ||
+                  "-"}
+              </h1>
             </div>
             <p className="text-sm text-muted-foreground">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel
@@ -122,7 +172,8 @@ const SingleUserPage = () => {
               in, quis quia.
             </p>
           </div>
-          {/* INFORMATION CONTAINER */}
+
+          {/* User Information */}
           <div className="bg-primary-foreground p-4 rounded-lg">
             <div className="flex items-center justify-between">
               <h1 className="text-xl font-semibold">User Information</h1>
@@ -133,6 +184,7 @@ const SingleUserPage = () => {
                 <EditUser />
               </Sheet>
             </div>
+
             <div className="space-y-4 mt-4">
               <div className="flex flex-col gap-2 mb-8">
                 <p className="text-sm text-muted-foreground">
@@ -142,34 +194,40 @@ const SingleUserPage = () => {
               </div>
               <div className="flex items-center gap-2">
                 <span className="font-bold">Full name:</span>
-                <span>John Doe</span>
+                <span>
+                  {data?.firstName + " " + data?.lastName ||
+                    data?.username ||
+                    "-"}
+                </span>
               </div>
               <div className="flex items-center gap-2">
                 <span className="font-bold">Email:</span>
-                <span>john.doe@gmail.com</span>
+                <span>{data.emailAddresses[0]?.emailAddress || "-"}</span>
               </div>
               <div className="flex items-center gap-2">
                 <span className="font-bold">Phone:</span>
-                <span>+1 234 5678</span>
+                <span>{data.phoneNumbers[0]?.phoneNumber || "-"}</span>
               </div>
               <div className="flex items-center gap-2">
-                <span className="font-bold">Address:</span>
-                <span>123 Main St</span>
+                <span className="font-bold">Role:</span>
+                <span>{String(data.publicMetadata?.role) || "user"}</span>
               </div>
               <div className="flex items-center gap-2">
-                <span className="font-bold">City:</span>
-                <span>New York</span>
+                <span className="font-bold">Status:</span>
+                <span>{data.banned ? "banned" : "active"}</span>
               </div>
             </div>
+
             <p className="text-sm text-muted-foreground mt-4">
-              Joined on 2025.01.01
+              {new Date(data.createdAt).toLocaleDateString("en-US")}
             </p>
           </div>
         </div>
-        {/* RIGHT */}
+
+        {/* RIGHT COLUMN*/}
         <div className="w-full xl:w-2/3 space-y-6">
           
-          {/* CHART CONTAINER */}
+          {/* Chart */}
           <div className="bg-primary-foreground p-4 rounded-lg">
             <h1 className="text-xl font-semibold">User Activity</h1>
             <AppLineChart />
