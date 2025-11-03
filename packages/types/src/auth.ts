e8c483fbdbb48a964fdde374801551c9e482405d
Jorge Matos <jisapjisap@gmail.com>
2025-11-03 16:02:11 +0100

    feat(admin): refactor AddUser component to utilize UserFormSchema for form validation, integrate user creation with react-query, and enhance user experience with toast notifications

--- packages/types/src/auth.ts
+++ packages/types/src/auth.ts
@@ -1,3 +1,4 @@
+import z from "zod";
 
 
 export interface CustomJwtSessionClaims {
@@ -7,3 +8,22 @@ export interface CustomJwtSessionClaims {
 }
 
 
+export const UserFormSchema = z.object({
+  firstName: z
+    .string({ message: "First name is required!" })
+    .min(2, { message: "First name must be at least 2 characters!" })
+    .max(50),
+  lastName: z
+    .string({ message: "Last name is required!" })
+    .min(2, { message: "Last name must be at least 2 characters!" })
+    .max(50),
+  username: z
+    .string({ message: "Username is required!" })
+    .min(2, { message: "Username must be at least 2 characters!" })
+    .max(50),
+  emailAddress: z.array(z.string({ message: "Email address is required!" })),
+  password: z
+    .string({ message: "Password is required!" })
+    .min(8, { message: "Password must be at least 8 characters!" })
+    .max(50),
+});
